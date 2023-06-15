0e75ef376dbcd1b7b5b2b01a4790731509c3a181
Timofiy Safronov <tsafr@softserveinc.com>
2023-06-15 10:07:13 +0300

    UC#10 Implement regex tests

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Sample inputs and edge cases
+string[] inputs = {
+            "Abcdef!1",         // Valid input
+            "Short1!",          // Matching input length
+            "Toolonginput123!", // Exceeding max length
+            "missinguppercase1!", // Missing uppercase
+            "MISSINGLOWERCASE1!",  // Missing lowercase
+            "MissingSpecialChar1", // Missing special character
+            "Missingdigit!",       // Missing digit
+            "  ExtraSpaces 1!  ",   // Excess whitespace characters
+            "abc123",             // Missing uppercase and special character
+            "ABC123",             // Missing lowercase and special character
+            "Abcdefg",            // Missing digit and special character
+            "ABCD1231",            // Missing lowercase and special character
+        };
+
+int maxLength = 15;
+
+foreach (string input in inputs)
+{
+    bool isValid = StringValidator.ValidateString(input, maxLength);
+
+    Console.WriteLine($"Input: {input}");
+    Console.WriteLine($"Valid: {isValid}");
+    Console.WriteLine();
+}
---
+++ StringValidator.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+public class StringValidator
+{
+    public static bool ValidateString(string input, int maxLegth)
+    {
+        string pattern = @"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!""#$%&'()*+,-./:;<=>?@[\]^_`{|}~])\S{1,%s}$";
+        pattern = pattern.Replace("%s", maxLegth.ToString());
+        Regex regex = new Regex(pattern);
+
+        return regex.IsMatch(input);
+    }
+}
